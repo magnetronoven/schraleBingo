9ced28f15ef1f1ca969c8c01299c2bfadb27a85e
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2019-06-25 14:16:05 +0200

    added styling and fetch functionality

--- App.js
+++ App.js
@@ -1,48 +1,116 @@
-import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, Image, StyleSheet, Dimensions } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import BingoCard from "./components/BingoCard";
+// var Sound = require('react-native-sound');
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
 
-  const data = [
-    {
-      name: "Alec",
-      color: "#FF0000",
-    },
-    {
-      name: "Jeroen",
-      color: "#00FF00",
-    }
-  ];
+  let [players, setPlayers] = useState([]);
+
+
+  useEffect(() => {
+    navigation.setParams({ 
+      hideHeader: true,
+    }) 
+    console.log("Reload bingoCardss")
+    fetchPlayers().then(data => setPlayers(data))
+
+    // Music!
+    // Sound.setCategory('Playback');
+    // var whoosh = new Sound('./assets/erika.mp3', Sound.MAIN_BUNDLE, (error) => {
+    //   if (error) {
+    //     console.log('failed to load the sound', error);
+    //     return;
+    //   }
+    //   // loaded successfully
+    //   console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+      
+    //   // Play the sound with an onEnd callback
+    //   whoosh.play((success) => {
+    //     if (success) {
+    //       console.log('successfully finished playisng');
+    //     } else {
+    //       console.log('playback failed due to audio decoding errors')
+    //     }
+    //   });
+    // });
+  }, []); 
+
+  const fetchPlayers = () => {
+    return fetch('https://thierry.sytes.net/schralebingo/api/players')
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => {
+        console.error(err);
+        console.log()
+      })
+  }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      {data.map((object, i) => (
-        <View 
-          key={i}
-          style={{marginBottom: 20, marginLeft: 30, marginRight: 30}}>
-          <Button
-            containerViewStyle={{width: '100%', marginLeft: 0}}
-            title={object.name}
-            onPress={() => props.navigation.navigate('BingoCard', {object})}
-            color={object.color}
-          />
-        </View>
-      ))}
+    <View style={{ flex: 1, justifyContent: 'center' , backgroundColor: "black"}}>
+      <View style={style.imageContainer}>
+        <Image
+          style={style.image}
+          resizeMode={'contain'}
+          source={require('./assets/schraleBingoLogo.png')}
+        />
+      </View>
+      <View style={style.buttonContainer}>
+        {players.map((player, i) => (
+          <View 
+            key={i}
+            style={style.buttons}>
+            <Button
+              containerViewStyle={{width: '100%', marginLeft: 0}}
+              title={player.name}
+              onPress={() => navigation.navigate('BingoCard', {player})}
+              color={player.color}
+            />
+          </View>
+        ))}
+      </View>
     </View>
   );
   
 }
 
-HomeScreen.navigationOptions = ({ navigation }) => ({
-  title: "Schrale Bingo!"
+// HomeScreen.navigationOptions = ({ navigation }) => ({
+//   title: "Schrale Bingo!"
+// });
+
+const style = StyleSheet.create({
+  image: {
+
+      width: "100%",
+      
+  },
+  imageContainer:{
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+  },
+  buttons: {
+    marginBottom: 20,
+    marginLeft: 30, 
+    marginRight: 30
+  },
+  buttonContainer: {
+    flex: 1
+  }
 });
 
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    BingoCard: BingoCard
+    BingoCard: BingoCard,
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        title: 'Home',
+        header: null //this will hide the header
+      },
+    },
   },
   {
     initialRouteName: "Home",
@@ -53,9 +121,13 @@ const AppNavigator = createStackNavigator(
       headerTintColor: '#fff',
       headerTitleStyle: {
         fontWeight: 'bold',
-      }
-    }
+      },
+    },
+    cardStyle: {
+      backgroundColor: 'rgba(0,0,0,0.5)',
+    },
   }
 );
+
 
 export default createAppContainer(AppNavigator);
--- components/BingoCard.js
+++ components/BingoCard.js
@@ -1,23 +1,46 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet  } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 const BingoCard = (props) => {
-  const object = props.navigation.getParam('object', null);
+  const player = props.navigation.getParam('player', null);
 
+  let [bingoCards, setBingoCards] = useState([
+    {"player":player.name,"text":"","crossed":0},
+    {"player":player.name,"text":"","crossed":0},
+    {"player":player.name,"text":"","crossed":0},
+    {"player":player.name,"text":"","crossed":0},
+    {"player":player.name,"text":"","crossed":0},
+    {"player":player.name,"text":"","crossed":0},
+    {"player":player.name,"text":"","crossed":0},
+    {"player":player.name,"text":"","crossed":0},
+  ]);
+
+  useEffect(() => {
+    console.log("Reload bingoCards")
+    fetchBingoCards().then(data => setBingoCards(data))
+  }, []);
+
+  const fetchBingoCards = () => {
+    return fetch('https://thierry.sytes.net/schralebingo/api/bingocards')
+      .then((res) => res.json())
+      .then((data) => data.filter((card) => {
+        return card.player == player.name
+      }))
+      .catch((err) => {
+        console.error(err);
+      })
+  }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center'}}>
+    <View style={{ flex: 1, alignItems: 'center', backgroundColor: "black"}}>
       <View style={style.container}>
-        <View style={style.box}><View style={style.textContainer}><Text style={style.text}>Verwisseld Hongarije met Bulgarije </Text></View></View>
-        <View style={style.box}><View style={style.textContainer}><Text style={style.text}>Dit wordt een veels te lange zin om het systeem te testen</Text></View></View>
-        <View style={style.box}><View style={style.textContainer}><Text style={style.text}>Box Box Box Box Box Box Box Box Box Box Box </Text></View></View>
-        <View style={style.box}><View style={style.textContainer}><Text style={style.text}>Box Box Box Box Box Box Box Box Box Box Box </Text></View></View>
-        <View style={style.box}><Text style={style.middleText}>Schrale Stanus Bingo!</Text></View>
-        <View style={style.box}><View style={style.textContainer}><Text style={style.text}>Box Box Box Box Box Box Box Box Box Box Box </Text></View></View>
-        <View style={style.box}><View style={style.textContainer}><Text style={style.text}>Box Box Box Box Box Box Box Box Box Box Box </Text></View></View>
-        <View style={style.box}><View style={style.textContainer}><Text style={style.text}>Box Box Box Box Box Box Box Box Box Box Box </Text></View></View>
-        <View style={style.box}><View style={style.textContainer}><Text style={style.text}>Box Box Box Box Box Box Box Box Box Box Box </Text></View></View>
+        {
+          bingoCards.map((card, i) => {
+            if(i == 4) return <View key={i} style={style.box}><Text style={style.middleText}>Schrale Stanus Bingo!</Text></View>;
+            return <View key={i} style={[style.box, card.crossed ? style.crossed : null]}><View style={style.textContainer}><Text style={[style.text, card.crossed ? style.crossed : null]}>{card.text}</Text></View></View>;
+          })
+        }
       </View>
     </View>
   );
@@ -26,40 +49,51 @@ const BingoCard = (props) => {
 
 const style = StyleSheet.create({
   container: {
-    width: 350,
+    width: "90%",
     height: 300,
     marginTop: 20,
     flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
-    
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 1,
+    margin: 0
   },
   box: {
-    width: "33%",
+    width: "32%",
     height: 100,
     borderWidth: 1,
-    backgroundColor: "#fcf82d"
+    backgroundColor: "#333333",
+    marginTop: 5,
+    borderRadius:10,
   },
   textContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    color: "white"
   },
   middleText: {
     fontWeight: "bold",
     fontSize: 26,
     textAlign: "center",
-    fontFamily: "Roboto"
+    fontFamily: "Roboto",
+    color: "white"
   },
   text: {
-    textAlign: "center"
+    textAlign: "center",
+    color: "white"
+  },
+  crossed: {
+    backgroundColor: "#4fe01a"
   }
 });
 
 BingoCard.navigationOptions = ({ navigation }) => ({
-  title: navigation.getParam("object", null).name,
+  title: navigation.getParam("player", null).name+"'s Bingo Kaart",
   headerStyle: {
-    backgroundColor: navigation.getParam("object", null).color,
+    backgroundColor: navigation.getParam("player", null).color,
   },
 });
 
